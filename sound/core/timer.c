86650232d8ff2c2b470356c7837fdc3f2cfa8516
Takashi Iwai <tiwai@suse.de>
2016-01-30 23:09:08 +0100

    ALSA: timer: Fix link corruption due to double start or stop
    [ Upstream commit f784beb75ce82f4136f8a0960d3ee872f7109e09 ]
    
    Although ALSA timer code got hardening for races, it still causes
    use-after-free error.  This is however rather a corrupted linked list,
    not actually the concurrent accesses.  Namely, when timer start is
    triggered twice, list_add_tail() is called twice, too.  This ends
    up with the link corruption and triggers KASAN error.
    
    The simplest fix would be replacing list_add_tail() with
    list_move_tail(), but fundamentally it's the problem that we don't
    check the double start/stop correctly.  So, the right fix here is to
    add the proper checks to snd_timer_start() and snd_timer_stop() (and
    their variants).
    
    BugLink: http://lkml.kernel.org/r/CACT4Y+ZyPRoMQjmawbvmCEDrkBD2BQuH7R09=eOkf5ESK8kJAw@mail.gmail.com
    Reported-by: Dmitry Vyukov <dvyukov@google.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>

--- sound/core/timer.c
+++ sound/core/timer.c
@@ -478,24 +478,24 @@ return result;
 static int snd_timer_start_slave(struct snd_timer_instance *timeri,
 			 bool start)
 {
-unsigned long flags;
+	unsigned long flags;
 
-spin_lock_irqsave(&slave_active_lock, flags);
-if (timeri->flags & SNDRV_TIMER_IFLG_RUNNING) {
+	spin_lock_irqsave(&slave_active_lock, flags);
+	if (timeri->flags & SNDRV_TIMER_IFLG_RUNNING) {
+		spin_unlock_irqrestore(&slave_active_lock, flags);
+		return -EBUSY;
+	}
+	timeri->flags |= SNDRV_TIMER_IFLG_RUNNING;
+	if (timeri->master && timeri->timer) {
+		spin_lock(&timeri->timer->lock);
+		list_add_tail(&timeri->active_list,
+			      &timeri->master->slave_active_head);
+		snd_timer_notify1(timeri, start ? SNDRV_TIMER_EVENT_START :
+				  SNDRV_TIMER_EVENT_CONTINUE);
+		spin_unlock(&timeri->timer->lock);
+	}
 	spin_unlock_irqrestore(&slave_active_lock, flags);
-	return -EBUSY;
-}
-timeri->flags |= SNDRV_TIMER_IFLG_RUNNING;
-if (timeri->master && timeri->timer) {
-	spin_lock(&timeri->timer->lock);
-	list_add_tail(&timeri->active_list,
-		      &timeri->master->slave_active_head);
-	snd_timer_notify1(timeri, start ? SNDRV_TIMER_EVENT_START :
-			  SNDRV_TIMER_EVENT_CONTINUE);
-	spin_unlock(&timeri->timer->lock);
-}
-spin_unlock_irqrestore(&slave_active_lock, flags);
-return 1; /* delayed start */
+	return 1; /* delayed start */
 }
 
 /* stop/pause a master timer */
