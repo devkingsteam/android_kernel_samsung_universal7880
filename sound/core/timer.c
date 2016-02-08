cb3b506c48fd943d6ef51161ca4a751259a8d233
Takashi Iwai <tiwai@suse.de>
2016-02-08 17:36:25 +0100

    ALSA: timer: Fix wrong instance passed to slave callbacks
    [ Upstream commit 117159f0b9d392fb433a7871426fad50317f06f7 ]
    
    In snd_timer_notify1(), the wrong timer instance was passed for slave
    ccallback function.  This leads to the access to the wrong data when
    an incompatible master is handled (e.g. the master is the sequencer
    timer and the slave is a user timer), as spotted by syzkaller fuzzer.
    
    This patch fixes that wrong assignment.
    
    BugLink: http://lkml.kernel.org/r/CACT4Y+Y_Bm+7epAb=8Wi=AaWd+DYS7qawX52qxdCfOfY49vozQ@mail.gmail.com
    Reported-by: Dmitry Vyukov <dvyukov@google.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>

--- sound/core/timer.c
+++ sound/core/timer.c
@@ -393,33 +393,33 @@ return 0;
 
 static void snd_timer_notify1(struct snd_timer_instance *ti, int event)
 {
-struct snd_timer *timer;
-unsigned long resolution = 0;
-struct snd_timer_instance *ts;
-struct timespec tstamp;
+	struct snd_timer *timer;
+	unsigned long resolution = 0;
+	struct snd_timer_instance *ts;
+	struct timespec tstamp;
 
-if (timer_tstamp_monotonic)
-	ktime_get_ts(&tstamp);
-else
-	getnstimeofday(&tstamp);
-if (snd_BUG_ON(event < SNDRV_TIMER_EVENT_START ||
-	       event > SNDRV_TIMER_EVENT_PAUSE))
-	return;
-if (event == SNDRV_TIMER_EVENT_START ||
-    event == SNDRV_TIMER_EVENT_CONTINUE)
-	resolution = snd_timer_resolution(ti);
-if (ti->ccallback)
-	ti->ccallback(ti, event, &tstamp, resolution);
-if (ti->flags & SNDRV_TIMER_IFLG_SLAVE)
-	return;
-timer = ti->timer;
-if (timer == NULL)
-	return;
-if (timer->hw.flags & SNDRV_TIMER_HW_SLAVE)
-	return;
-list_for_each_entry(ts, &ti->slave_active_head, active_list)
-	if (ts->ccallback)
-		ts->ccallback(ts, event + 100, &tstamp, resolution);
+	if (timer_tstamp_monotonic)
+		ktime_get_ts(&tstamp);
+	else
+		getnstimeofday(&tstamp);
+	if (snd_BUG_ON(event < SNDRV_TIMER_EVENT_START ||
+		       event > SNDRV_TIMER_EVENT_PAUSE))
+		return;
+	if (event == SNDRV_TIMER_EVENT_START ||
+	    event == SNDRV_TIMER_EVENT_CONTINUE)
+		resolution = snd_timer_resolution(ti);
+	if (ti->ccallback)
+		ti->ccallback(ti, event, &tstamp, resolution);
+	if (ti->flags & SNDRV_TIMER_IFLG_SLAVE)
+		return;
+	timer = ti->timer;
+	if (timer == NULL)
+		return;
+	if (timer->hw.flags & SNDRV_TIMER_HW_SLAVE)
+		return;
+	list_for_each_entry(ts, &ti->slave_active_head, active_list)
+		if (ts->ccallback)
+			ts->ccallback(ts, event + 100, &tstamp, resolution);
 }
 
 /* start/continue a master timer */
