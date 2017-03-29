8313ca8728b791fb91fd9ec4f3f4cd349b56ab04
Andrey Konovalov <andreyknvl@google.com>
2017-03-29 16:11:22 +0200

    net/packet: fix overflow in check for tp_reserve
    [ Upstream commit bcc5364bdcfe131e6379363f089e7b4108d35b70 ]
    
    When calculating po->tp_hdrlen + po->tp_reserve the result can overflow.
    
    Fix by checking that tp_reserve <= INT_MAX on assign.
    
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Acked-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/packet/af_packet.c
+++ net/packet/af_packet.c
@@ -3326,6 +3326,10 @@ packet_setsockopt(struct socket *sock, int level, int optname, char __user *optv
 		}
 		release_sock(sk);
 		return ret;
+		if (val > INT_MAX)
+			return -EINVAL;
+		po->tp_reserve = val;
+		return 0;
 	}
 	case PACKET_LOSS:
 	{
