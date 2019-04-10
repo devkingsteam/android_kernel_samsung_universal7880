06852d653295a9f07b43de5f6c3c5c84615682bc
YordiDR <yordi.derijcke@live.be>
2019-04-10 15:30:54 +0200

    fs/ext4/super.c - Fix mismerge

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -3821,6 +3821,8 @@ static int ext4_fill_super(struct super_block *sb, void *data, int silent)
 	    es->s_encryption_level) {
 		ext4_msg(sb, KERN_ERR, "Unsupported encryption level %d",
 			 es->s_encryption_level);
+	}
+
 	if (le16_to_cpu(sbi->s_es->s_reserved_gdt_blocks) > (blocksize / 4)) {
 		ext4_msg(sb, KERN_ERR,
 			 "Number of reserved GDT blocks insanely large: %d",
